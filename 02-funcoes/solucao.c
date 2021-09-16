adb8618cb35e4bf36855ee03b3dd63d0ef048393
guilhermecarvalhoacc <guilhermeaugustochaves@hotmail.com>
2021-09-16 18:02:19 -0300

    atv2 feita

--- 02-funcoes/solucao.c
+++ 02-funcoes/solucao.c
@@ -1,1 +1,18 @@
 // TODO implemente uma funcao chamada solucao aqui
+// registradores
+// %edi int a
+// %rsi long b
+
+
+
+
+
+int solucao(int a, long b, long *c){
+    a = a + a;
+    int res = b + 2*b;
+    res += a;
+    *c = res;
+    res = b + 4*b;
+    res += a;
+    return res;
+}
